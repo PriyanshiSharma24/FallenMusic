56059839c8883c8c123215e60ca9dc98d98b3371
𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>
2025-06-12 22:43:17 +0530

    Update start.py
    Signed-off-by: 𝖯𝗋𝗂𝗒𝖺𝗇𝗌𝗁𝗂 𝗌𝗁𝖺𝗋𝗆𝖺 ۵ 🇮🇳 <130631732+PriyanshiSharma24@users.noreply.github.com>

--- FallenMusic/Modules/start.py
+++ FallenMusic/Modules/start.py
@@ -68,7 +68,7 @@ async def fallen_st(_, message: Message):
                         [
                             InlineKeyboardButton(text="ʏᴏᴜᴛᴜʙᴇ", url=link),
                             InlineKeyboardButton(
-                                text="sᴜᴩᴩᴏʀᴛ", url=config.SUPPORT_CHAT
+                                text="sᴜᴩᴩᴏʀᴛ", url=f"https://t.me/+G0yTSKAKnsBlN2U9"
                             ),
                         ],
                     ]
